1fef4dda3d0692b7c02ff56b514bdc9dc21290f6
ddnn1510 <nndd.1510@gmail.com>
2024-05-02 22:13:02 +0700

    init project

---
+++ server.js
@@ -0,0 +1,39 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app = express();
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+import authRouter from './routes/authRouter.js';
+
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use('/api/v1/auth', authRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
+});
+
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5100;
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
