e6e60ca50a9d9f9a19a675ba3f9f1397d796a6a7
duongpd-0230 <phung.dinh.duong@sun-asterisk.com>
2024-05-16 01:56:39 +0700

    feat: Import questions from Google Docs

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,30 +1,84 @@
-// get random 30 questions from the database, randomize the order of the answers and send them to the client
-//
-import Question from '../models/QuestionModel';
+import Question from '../models/QuestionModel.js';
 import { google } from 'googleapis';
-const docs = google.docs({ version: 'v1', auth });
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: './utils/credentials.json',
+  scopes: ['https://www.googleapis.com/auth/documents'],
+});
 
 export const importQuestions = async (req, res) => {
   try {
-    // Get the document ID from the request
-    const docId = req.body.docId;
+    const data = await readGoogleDocs(
+      '12OdorWeyvDS1W1f7jomaKMng9OnkZl-QeLfwlYrrEaA'
+    );
 
-    // Fetch the document from Google Docs
-    const doc = await docs.documents.get({ documentId: docId });
+    let questionsData = extractTextRuns(data.body.content);
+    questionsData = questionsData.filter(
+      (object) => object.content.trim() !== ''
+    );
+    questionsData = questionsData.map((run) => {
+      // let content = run.content.replace(/^\s*[A-D]\.\s*|\d+\.\s*/, '');
+      let content = run.content.replace(/^\s*[A-D][.)]\s*|\d+[.)]\s*/, '');
+      let type = /^\s*\d+\./.test(run.content) ? 'question' : 'answer';
+      return { type, content, isAnswer: run.isAnswer };
+    });
 
-    // Parse the document content to extract questions
-    const questions = parseDocumentContent(doc.data.content);
+    let questions = [];
+    let currentQuestion = null;
+    questionsData.forEach((item) => {
+      if (item.type === 'question') {
+        currentQuestion = {
+          question: item.content,
+          answers: [],
+          correct: null,
+        };
+        questions.push(currentQuestion);
+      } else if (item.type === 'answer' && item.content.trim() !== '') {
+        currentQuestion.answers.push(item.content);
+        if (item.isAnswer) {
+          currentQuestion.correct = currentQuestion.answers.length - 1;
+        }
+      }
+    });
 
-    // Save the questions to your database
-    await saveQuestionsToDatabase(questions);
+    await Question.insertMany(questions);
 
-    res.status(200).json({ msg: 'Questions imported successfully' });
+    res.status(200).json(questions);
   } catch (error) {
+    console.log(error.message);
     res
       .status(500)
       .json({ msg: 'An error occurred while importing questions', error });
   }
 };
+
+function extractTextRuns(obj) {
+  let textRuns = [];
+
+  for (let key in obj) {
+    if (key === 'textRun') {
+      let content = obj[key].content;
+      let isAnswer = obj[key].textStyle.bold;
+      textRuns.push({ content, isAnswer });
+    } else if (typeof obj[key] === 'object') {
+      textRuns = textRuns.concat(extractTextRuns(obj[key]));
+    }
+  }
+
+  return textRuns;
+}
+
+async function readGoogleDocs(documentId) {
+  try {
+    const docs = google.docs({ version: 'v1', auth }); // Create a Google Docs API client
+
+    // Retrieve the document content
+    const response = await docs.documents.get({ documentId }); // ID of the document to read
+    return response.data; // Return the document data
+  } catch (error) {
+    console.error('error', error); // Log any errors that occur
+  }
+}
 
 export const getListQuestions = async (req, res) => {
   try {
